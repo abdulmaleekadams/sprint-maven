1ac47d7bacce7bccc0de63f2caa3f4de82f0040c
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-09-08 10:30:26 +0100

    Fix build error

--- actions/schema.ts
+++ actions/schema.ts
@@ -134,6 +134,7 @@ export const UpdateCardFormSchema = z.object({
   labels: z.array(z.string()).optional(),
   point: z.number().min(1).nullable().optional(),
   priority: z.number().min(1).nullable().optional(),
+  severity: z.string().nullable().optional(),
 });
 
 export const CopyCardSchema = z.object({
